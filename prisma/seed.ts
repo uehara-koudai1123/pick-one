b9e1566f00ebde404896df7fe4c4376716b02a62
上原孝大 <maxkodayn@ezweb.ne.jp>
2026-03-06 21:39:52 +0900

    ローカルセットアップ

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const prisma = new PrismaClient();
 
